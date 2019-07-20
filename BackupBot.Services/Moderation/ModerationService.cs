43826b404eb8f499a7082e4ddf89805e8e3ff714
Julian <495328jf@eur.nl>
2019-07-20 19:59:59 +0200

    finalllyyyy

---
+++ BackupBot.Services/Moderation/ModerationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BackupBot.Data.Entities;
+using BackupBot.Data.Repositories;
+using BackupBot.Services.Infrastructure;
+
+namespace BackupBot.Services.Moderation
+{
+    public class ModerationService : IModerationService
+    {
+        private readonly INoteRepository _repository;
+
+        public ModerationService(INoteRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task AddNoteAsync(ulong userId, string description,
+            ulong createdBy, DateTime dateCreated, NoteTypes type)
+        {
+            INote note = new Note(userId, description, createdBy, dateCreated, type);
+            await _repository.AddNoteAsync(note);
+        }
+
+    }
+}
