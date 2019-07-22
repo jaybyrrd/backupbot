d6c376355e6a64564adedc1a1450960221c7eec1
RyanThiele <dynamensions@live.com>
2019-07-22 18:44:03 -0400

    Files have been moved.

---
+++ BackupBot.Services/BackupService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BackupBot.Services
+{
+    class BackupRepository : IBackupService
+    {
+        public Task AddMessageAsync(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetMessageByChannelName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
