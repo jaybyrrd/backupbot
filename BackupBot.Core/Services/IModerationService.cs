6b6266ad32934976ccb815e30ff4f416571a96a1
Julian <495328jf@eur.nl>
2019-07-23 15:45:15 +0200

    again commands

---
+++ BackupBot.Core/Services/IModerationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BackupBot.Models;
+
+namespace BackupBot.Core.Services
+{
+    public interface IModerationService
+    {
+        /// <summary>
+        /// Add a note to the user's history.
+        /// </summary>
+        /// <param name="note"></param>
+        /// <returns></returns>
+        Task<bool> AddNoteAsync(INote note);
+
+        /// <summary>
+        /// Delete a note from the user's history.
+        /// </summary>
+        /// <param name="id">Note ID</param>
+        /// <returns></returns>
+        Task<bool> DeleteNoteAsync(int id);
+
+        /// <summary>
+        /// Get all notes of user.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        Task<IList<INote>> GetNotesAsync(User user);
+
+        
+    }
+}
---
+++ BackupBot.Models/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BackupBot.Models
+{
+    public class Enums
+    {
+        [Flags]
+        public enum UserRights
+        {
+            Mvp = 0,
+            Mod = 1,
+            Admin = 2
+        }
+
+        public enum NoteTypes
+        {
+            Little,
+            Severe,
+            LastWarning
+        }
+    }
+}
